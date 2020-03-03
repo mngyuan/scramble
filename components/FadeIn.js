7eb760fc5049e07f449325452d7d98085bf87f1d
Kevin Lee <me@mngyuan.com>
2020-03-03 15:44:31 -0800

    move intersection observer import

--- components/FadeIn.js
+++ components/FadeIn.js
@@ -1,3 +1,4 @@
+import 'intersection-observer';
 import React, {useState, useEffect} from 'react';
 import {InView} from 'react-intersection-observer';
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import 'intersection-observer';
-
 import Scramble from '../components/Scramble';
 import FadeInGroup from '../components/FadeInGroup';
 
