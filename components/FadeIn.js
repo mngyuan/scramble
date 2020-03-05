5c767e9d94980fd06ed9da35e34230bdedeb8f09
Kevin Lee <kevinmlee.x@gmail.com>
2020-03-05 14:13:16 -0800

    add disabled, timeLen props to FadeIn

--- components/FadeIn.js
+++ components/FadeIn.js
@@ -3,7 +3,10 @@ import React, {useState, useEffect} from 'react';
 import {InView} from 'react-intersection-observer';
 
 const FadeIn = props => {
-  const {delay = 0, children} = props;
+  const {delay = 0, children, timeLen = 600, disabled = false} = props;
+  if (disabled) {
+    return props.children;
+  }
   const [delayPassed, setDelayPassed] = useState(delay === 0);
   if (delay !== 0) {
     useEffect(() => {
@@ -19,7 +22,8 @@ const FadeIn = props => {
           <style jsx>{`
             .fade-in-on-load {
               opacity: ${inView && delayPassed ? 1 : 0};
-              transition: opacity 0.6s ease, top 0.6s ease;
+              transition: opacity ${timeLen / 1000}s ease,
+                top ${timeLen / 1000}s ease;
               position: relative;
               top: ${inView && delayPassed ? 0 : 12}px;
               will-change: opacity, top;
