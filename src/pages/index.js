1650f31c39d3dabe10d7239cc77ec0734919a379
Kevin Lee <kevinmlee.x@gmail.com>
2020-05-20 18:08:52 -0700

    update blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import {PRIMARY_COLOR, DEFAULT_BG} from '../const';
 let randPickNew;
 (() => {
   let last;
-  randPickNew = l => {
+  randPickNew = (l) => {
     let i = Math.floor(Math.random() * l.length);
     while (i === last && l.length > 1) {
       i = Math.floor(Math.random() * l.length);
@@ -20,9 +20,25 @@ let randPickNew;
   };
 })();
 
-const okf = o => Object.keys(o).filter(k => o[k]);
-const classNames = o => okf(o).join(' ');
+const okf = (o) => Object.keys(o).filter((k) => o[k]);
+const classNames = (o) => okf(o).join(' ');
 
+const ITEMS = {
+  freelance: [],
+  'graphic design': [
+    'media/DREAMING excerpt.jpeg',
+    '/media/scan.jpg',
+    '/media/how to have friends and still feel alone for web-05.png',
+  ],
+  'creative tech': [
+    '/media/KYLIN-excerpt.mp4',
+    '/media/stand still excerpt.mp4',
+    '/media/bw_6 excerpt.mp4',
+    '/media/000025.jpg',
+    '/media/S398787-R3-E074.jpg',
+  ],
+  software: ['/media/oww2.mp4', '/media/oww3.mp4'],
+};
 const MUSIC_BGS = [
   '/media/first.mp3',
   '/media/flight1.mp3',
@@ -30,30 +46,16 @@ const MUSIC_BGS = [
   '/media/19-04_abeepitidybeepbapboobee.mp3',
   '/media/20-07 PO-20 ARCADE.mp3',
 ];
-const VISUAL_BGS = [
-  '/media/000025.jpg',
-  '/media/S398787-R3-E074.jpg',
-  '/media/KYLIN-excerpt.mp4',
-  '/media/Screen Shot 2019-11-19 at 6.13.48 PM.jpeg',
-  '/media/scan.jpg',
-  '/media/how to have friends and still feel alone for web-05.png',
-  '/media/stand still excerpt.mp4',
-  '/media/bw_6 excerpt.mp4',
-];
-const WEB_BGS = ['/media/oww2.mp4', '/media/oww3.mp4'];
 
-const Background = props => {
-  const curVisualBgSrc =
-    props.viewing === 'visual' ? randPickNew(VISUAL_BGS) : null;
-  const curWebBgSrc = props.viewing === 'web' ? randPickNew(WEB_BGS) : null;
-  const curMusicBgSrc =
-    props.viewing === 'music' ? randPickNew(MUSIC_BGS) : null;
-  const curVisualBg = curVisualBgSrc ? (
-    ['mp4'].includes(curVisualBgSrc.split('.').slice(-1)[0]) ? (
+const Background = ({viewing}) => {
+  const backgroundSrc =
+    viewing !== 'landing' ? randPickNew(ITEMS[viewing]) : null;
+  const bg = backgroundSrc ? (
+    ['mp4'].includes(backgroundSrc.split('.').slice(-1)[0]) ? (
       <div>
         <video
           className="visualBg"
-          src={curVisualBgSrc}
+          src={backgroundSrc}
           type="video/mp4"
           preload="auto"
           loop
@@ -80,7 +82,7 @@ const Background = props => {
       </div>
     ) : (
       <div>
-        <img src={curVisualBgSrc} className="visualBg" />
+        <img src={backgroundSrc} className="visualBg" />
         <style jsx>{`
           div {
             position: fixed;
@@ -101,105 +103,45 @@ const Background = props => {
       </div>
     )
   ) : null;
-  const curWebBg = curWebBgSrc ? (
-    <div>
-      <video
-        src={curWebBgSrc}
-        className="visualBg"
-        type="video/mp4"
-        preload="auto"
-        loop
-        playsInline
-        muted
-        autoPlay
-      />
-      <style jsx>{`
-        div {
-          position: fixed;
-          top: 20vh;
-          left: 20vw;
-          width: 60vw;
-          height: 60vh;
-          overflow: visible;
-        }
-        .hidden {
-          display: none;
-        }
-        .visualBg {
-          object-fit: cover;
-          width: 100%;
-        }
-      `}</style>
-    </div>
-  ) : null;
-  const curMusicBg = curMusicBgSrc ? (
-    <div>
-      <span>now playing: {curMusicBgSrc}</span>
-      <audio src={curMusicBgSrc} preload="auto" loop autoPlay />
-      <style jsx>{`
-        div {
-          position: fixed;
-          top: 20vh;
-          left: 20vw;
-          width: 60vw;
-          height: 60vh;
 
-          text-align: center;
-          color: white;
-        }
-        .hidden {
-          display: none;
-        }
-      `}</style>
-    </div>
-  ) : null;
-
-  return curVisualBg || curWebBg || curMusicBg;
+  return bg;
 };
 
 export default class Index extends React.Component {
   state = {
     viewing: 'landing',
-    clicked: false,
   };
 
   componentDidMount() {
-    MUSIC_BGS.forEach(src => (new Audio().src = src));
-    VISUAL_BGS.forEach(src => (new Image().src = src));
-    WEB_BGS.forEach(src => (new Image().src = src));
+    Object.values(ITEMS)
+      .flat()
+      .map((src) => (new Image().src = src));
   }
 
   render() {
     return (
       <>
         <Head>
-          {VISUAL_BGS.map(src => (
-            <link
-              key={src}
-              rel="preload"
-              href={src}
-              as={
-                ['mp4'].includes(src.split('.').slice(-1)[0])
-                  ? 'video'
-                  : 'image'
-              }
-              type={
-                ['mp4'].includes(src.split('.').slice(-1)[0])
-                  ? 'video/mp4'
-                  : 'image/jpeg'
-              }
-            />
-          ))}
-          {WEB_BGS.map(src => (
-            <link
-              key={src}
-              rel="preload"
-              href={src}
-              as="video"
-              type="video/mp4"
-            />
-          ))}
-          {MUSIC_BGS.map(src => (
+          {Object.values(ITEMS)
+            .flat()
+            .map((src) => (
+              <link
+                key={src}
+                rel="preload"
+                href={src}
+                as={
+                  ['mp4'].includes(src.split('.').slice(-1)[0])
+                    ? 'video'
+                    : 'image'
+                }
+                type={
+                  ['mp4'].includes(src.split('.').slice(-1)[0])
+                    ? 'video/mp4'
+                    : 'image/jpeg'
+                }
+              />
+            ))}
+          {MUSIC_BGS.map((src) => (
             <link
               key={src}
               rel="preload"
@@ -210,55 +152,51 @@ export default class Index extends React.Component {
           ))}
         </Head>
         <Background viewing={this.state.viewing} />
-        <Layout onPointerDown={() => this.setState({clicked: true})}>
+        <Layout>
           <FadeInGroup>
-            <Scramble s="Hello, my name is Kevin Lee." />
+            <div>
+              <Scramble s="Hello, I'm Kevin Lee, a " />
+              <span
+                className="primary"
+                onPointerDown={() => this.setState({viewing: 'freelance'})}
+                onPointerOut={() => this.setState({viewing: 'landing'})}
+              >
+                freelance web developer
+              </span>
+              <Scramble s=" and " />
+              <span
+                className="primary"
+                onPointerDown={() => this.setState({viewing: 'creative tech'})}
+                onPointerOut={() => this.setState({viewing: 'landing'})}
+              >
+                creative technologist
+              </span>
+              <Scramble s="." />
+            </div>
             <br />
-            <Scramble s="When I'm freelancing or making art, I go by my Chinese name, MNGYUAN." />
-            <Scramble s="It means dream far." />
+            <Scramble s="I use my Chinese name, MNGYUAN, for my work. It means dream far." />
             <br />
             <div delayInit={1200}>
-              <Scramble s="I'm passionate about " />
+              <Scramble s="I'm passionate about creating expressive & empathetic digital experiences, and harnessing novel tech to do so. In my personal time I'm currently investigating ways to creative concious and thoughtful art at the intersection of " />
               <span
                 className="primary"
-                onPointerOver={() =>
-                  this.state.clicked ? this.setState({viewing: 'visual'}) : null
-                }
-                onPointerDown={() =>
-                  this.setState({viewing: 'visual', clicked: true})
-                }
+                onPointerDown={() => this.setState({viewing: 'graphic design'})}
                 onPointerOut={() => this.setState({viewing: 'landing'})}
               >
-                visual storytelling
+                graphic design
               </span>
-              <Scramble s=", " />
+              <Scramble s=" and " />
               <span
                 className="primary"
-                onPointerOver={() =>
-                  this.state.clicked ? this.setState({viewing: 'web'}) : null
-                }
-                onPointerDown={() =>
-                  this.setState({viewing: 'web', clicked: true})
-                }
+                onPointerDown={() => this.setState({viewing: 'software'})}
                 onPointerOut={() => this.setState({viewing: 'landing'})}
               >
-                next-gen frontend web tech
+                software
               </span>
-              <Scramble s=", and " />
-              <span
-                className="primary"
-                onPointerOver={() =>
-                  this.state.clicked ? this.setState({viewing: 'music'}) : null
-                }
-                onPointerDown={() =>
-                  this.setState({viewing: 'music', clicked: true})
-                }
-                onPointerOut={() => this.setState({viewing: 'landing'})}
-              >
-                music production
-              </span>
-              <Scramble s=". I'm currently looking for new opportunities - freelance web/graphic design and full time software and software/design hybrid roles." />
+              <Scramble s="." />
             </div>
+            <br />
+            <Scramble s="Let's talk!" />
             <br />
             <br />
             <br />
